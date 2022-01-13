509a91c61dc7c0340f5956352e8cc97669585fb2
Noshina Ninan <noshzann@gmail.com>
2022-01-13 15:18:56 -0500

    initial commit, till chapter 2.2

---
+++ SpringFrameworkinDepth/src/main/java/com/noshinaninan/SpringFrameworkinDepth/App.java
@@ -0,0 +1,24 @@
+package com.noshinaninan.SpringFrameworkinDepth;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.noshinaninan.SpringFrameworkinDepth.config.ApplicationConfig;
+import com.noshinaninan.SpringFrameworkinDepth.service.OutputService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	 public static void main(String[] args) throws Exception {
+	        ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+	        OutputService outputService = context.getBean(OutputService.class);
+
+	        for (int i=0;i<5;i++){
+	            outputService.generateOutput("Noshina");
+	            Thread.sleep(5000);
+	        }
+	    }
+	}
